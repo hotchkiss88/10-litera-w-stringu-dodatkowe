8781e8b727a75df51e69876a78b175e35cff46a6
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-13 21:25:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let one = "Szkoła Programowania Akademia 108";
+let two = "a";
+
+
+
+function stringCount(txt) {
+    var storage = [];
+
+    if (typeof txt === "string") {
+        storage = txt.split('');
+    }
+    let score = [];
+    for (i = 0; i < storage.length; i++) {
+        score = storage.filter(element => element === two);
+
+    }
+
+    console.log("Litera \"a\" wystąpiła " +
+        score.length + " razy");
+}
+
+stringCount(one);
