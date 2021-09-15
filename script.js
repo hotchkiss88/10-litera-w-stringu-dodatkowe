34c6bb2024daf34ef853245bd7938a1d000a831c
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-15 12:07:34 +0000

    add .toLowerCase

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ let two = "a";
 
 
 function stringCount(txt) {
-    var storage = [];
+  
+  var storage = [];
+ 
 
-    if (typeof txt === "string") {
-        storage = txt.split('');
-    }
-    let score = [];
-    for (i = 0; i < storage.length; i++) {
-        score = storage.filter(element => element === two);
+  if (typeof txt === "string") {
+    storage = txt.toLowerCase().split('');
+  }
+  let score = [];
+  for (i = 0; i < storage.length; i++) {
+    score = storage.filter(element => element === two);
 
-    }
+  }
 
-    console.log("Litera \"a\" wystąpiła " +
-        score.length + " razy");
+  console.log("Litera \"a\" wystąpiła " +
+    score.length + " razy");
 }
 
 stringCount(one);
